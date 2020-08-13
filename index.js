298d04ee13d872a7bd4b2931ec6dd4037eb59c5f
Rajat Gupta <rajat456bansal@gmail.com>
2020-08-13 14:36:25 +0530

    index.js

--- index.js
+++ index.js
@@ -13,12 +13,7 @@ const PORT = process.env.PORT || 8081;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("*", (req, res) => {
-  let url = path.join(__dirname, '../client/build', 'index.html');
-  if (!url.startsWith('/app/')) // since we're on local windows
-    url = url.substring(1);
-  res.sendFile(url);
-});
+
 
 app.use("/api/auth", authRoutes);
 app.use(
