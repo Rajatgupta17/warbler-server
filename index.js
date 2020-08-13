3210bd5c9976bd2f694dc5fca68df929b239f197
Rajat Gupta <rajat456bansal@gmail.com>
2020-08-13 14:19:51 +0530

    index.js

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ const PORT = process.env.PORT || 8081;
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get("*", (req, res) => {
+  let url = path.join(__dirname, '../client/build', 'index.html');
+  if (!url.startsWith('/app/')) // since we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
+});
+
 app.use("/api/auth", authRoutes);
 app.use(
   "/api/users/:id/messages",
