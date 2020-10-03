fd32b4779ee4ebcc2cbedbd9fa99a62957eb33cf
Rajatgupta17 <59507964+Rajatgupta17@users.noreply.github.com>
2020-10-03 19:18:09 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.set('debug', true);
 mongoose.Promise=Promise;
-mongoose.connect("mongodb+srv://rajat456bansal:Rajat%401705@cluster0-w5iyn.mongodb.net/warbler?retryWrites=true&w=majority", { useNewUrlParser: true, keepAlive:true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://rajat456bansal:****@cluster0-w5iyn.mongodb.net/warbler?retryWrites=true&w=majority", { useNewUrlParser: true, keepAlive:true, useUnifiedTopology: true});
 module.exports.User=require("./user");
 module.exports.Message=require("./message");
